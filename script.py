5783b15318941f85ab567c48c16c994fb545b896
em <emvp84@gmail.com>
2014-09-24 17:12:31 -0400

    printing input

--- script.py
+++ script.py
@@ -4,4 +4,6 @@ import logging, sys, argparse
 
 parser = argparse.ArgumentParser(description='Upload Blackboard Logs to SFTP')
 parser.add_argument('-f', '--file', type=str, nargs=1, help='The file to read the configuration if we are using file.')
-parser.add_argument('-i', '--inline', type=str, nargs=1, help='We are reading the configuration from the input as variables.')
+
+args = parser.parse_args()
+print args.file
