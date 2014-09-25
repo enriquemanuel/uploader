3d3a2abcd469480268a278b5bb4cff72802a8065
em <emvp84@gmail.com>
2014-09-24 20:42:18 -0400

    added setting variables as well as validated from where the information is coming

--- script.py
+++ script.py
@@ -2,9 +2,13 @@
 import logging, sys, argparse
 
 def get_args():
+  # Define the parser
   parser = argparse.ArgumentParser(description='Upload Blackboard Logs to SFTP')
+  parser.add_argument('-d', '--debug', required=False, help='Enable debug output', action='store_true')
+  parser.add_argument('-l', '--log-file', nargs=1, required=False, help='File to log to (default = stdout)', dest='logfile', type=str)
+  parser.add_argument('-V', '--version', action='version', version="%(prog)s (version 0.1)")
 
-  # configuring subparsers for actions
+  # Configuring subparsers for actions
   subparsers = parser.add_subparsers(help='Select which Option File or CLI in order to do it correctly')
 
   # File Line Argument
@@ -15,10 +19,9 @@ def get_args():
   cli_parser = subparsers.add_parser('cli', help='Define the arguments in the same line of execution')
   cli_parser.add_argument('--sftp',required=True, type=str, help='SFTP server to connect to.')
   cli_parser.add_argument('--user',required=True, type=str, help='User used to connect to SFTP.')
-  cli_parser.add_argument('--id', required=True, type=str, help='ID file to be used with path eg: /Users/userid/.ssh/server_rsa')
-  cli_parser.add_argument('--port', type=str, help='Port to connect to SFTP, default is 22')
-  cli_parser.add_argument('--path', type=str, help='Path to drop the logs, default is /')
-  #now the logs
+  cli_parser.add_argument('--id', type=str, help='ID file to be used with path eg: /Users/userid/.ssh/server_rsa (default is ~/.ssh/id_rsa)')
+  cli_parser.add_argument('--port', type=str, help='Port to connect to SFTP (default is 22)')
+  cli_parser.add_argument('--path', type=str, help='Path to drop the logs (default is /)')
   cli_parser.add_argument('--bbservices', type=str, help='To include or not the bb-services.log')
   cli_parser.add_argument('--bbsql', type=int, help='To include or not the bb-services.log')
   cli_parser.add_argument('--bbauth', type=int, help='To include or not the bb-services.log')
@@ -27,13 +30,32 @@ def get_args():
   cli_parser.add_argument('--tomcataccess', type=str, help='To include or not the bb-services.log')
   cli_parser.add_argument('--tomcatstd', type=str, help='To include or not the bb-services.log')
 
-
-
-
-
-
+  # Get all the values in the args variable
   args = parser.parse_args()
-  return args
 
-args = get_args()
-print args
+  # define the defaults for Port / Path / Id
+
+
+  # Set all the Vars
+  return setVars(args)
+
+def setVars(args):
+  try:
+    args.file
+  except AttributeError: # file is not defined, so using CLI
+    if args.port is None:
+      args.port=22
+    if args.path is None:
+      args.path='/'
+    if args.id is None:
+      args.id='~/.ssh/id_rsa'
+  else:
+    readVars(args.file)
+
+
+# Read the vars from the file
+def readVars(filename):
+  print filename
+
+# Call the function get_args to define all the arguments
+get_args()
